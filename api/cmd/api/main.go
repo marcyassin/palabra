b3f2ed1a195c85da6d41438c61b3fc5e349d0c5b
Marc Yassin <marc@yass.in>
2025-10-15 00:23:35 -0400

    added postgres driver

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -1,27 +1,39 @@
 package main
 
 import (
+    "database/sql"
     "flag"
     "log"
     "net/http"
     "os"
+
+    _ "github.com/lib/pq"
 )
 
 type application struct {
     errorLog *log.Logger
     infoLog  *log.Logger
+    db       *sql.DB
 }
 
 func main() {
     addr := flag.String("addr", ":4000", "HTTP network address")
+    dsn := flag.String("dsn", "postgres://bookprep_user:secret@localhost:5432/bookprep?sslmode=disable", "Postgres data source name")
     flag.Parse()
 
     infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
     errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+    db, err := openDB(*dsn)
+    if err != nil {
+        errorLog.Fatal(err)
+    }
+    defer db.Close()
+
     app := &application{
         errorLog: errorLog,
         infoLog:  infoLog,
+        db:       db,
     }
 
     srv := &http.Server{
@@ -31,7 +43,18 @@ func main() {
     }
 
     infoLog.Printf("Starting server on %s", *addr)
-    err := srv.ListenAndServe()
+    err = srv.ListenAndServe()
     errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+    db, err := sql.Open("postgres", dsn)
+    if err != nil {
+        return nil, err
+    }
+    if err = db.Ping(); err != nil {
+        return nil, err
+    }
+    return db, nil
 }
     
--- api/go.mod
+++ api/go.mod
@@ -1,3 +1,5 @@
 module marcyassin/bookprep
 
 go 1.25.2
+
+require github.com/lib/pq v1.10.9 // indirect
