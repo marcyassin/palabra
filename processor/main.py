e7c63d1576e36ffb9eea6371bd4c629e9a918020
Marc Yassin <marc@yass.in>
2025-10-16 11:05:04 -0400

    Update main.py

--- processor/main.py
+++ processor/main.py
@@ -12,7 +12,7 @@ DB_URL = os.getenv("DATABASE_URL", "postgres://palabra:secret@localhost:5432/pal
 MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "localhost:9000")
 MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "minioadmin")
 MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY", "minioadmin")
-MINIO_BUCKET = os.getenv("MINIO_BUCKET", "bookprep")
+MINIO_BUCKET = os.getenv("MINIO_BUCKET", "palabra")
 MINIO_USE_SSL = os.getenv("MINIO_SSL", "false").lower() == "true"
 
 logging.basicConfig(level=logging.INFO)
